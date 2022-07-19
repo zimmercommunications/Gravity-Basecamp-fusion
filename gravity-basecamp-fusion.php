e12c1975f919eb1849ff6712be43d191825977fa
Jesse Corkill <xxjescorkillxx@gmail.com>
2022-07-19 11:27:18 -0500

    Trimmed Controllers

---
+++ gravity-basecamp-fusion.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:  GravityForms Basecamp Fusion
+Description:  A WordPress plugin to optionally send GravityForms submissions to the BaseCamp API to turn into a pre-fromated job. 
+Version:      1.001
+Author:       Jesse Corkill 
+Author URI:   https://github.com/reckypoo
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
